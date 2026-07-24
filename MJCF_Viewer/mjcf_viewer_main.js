b0a1d4a26057ead858b7bd77e90d06f0826469f8
Artemio Araya Day <78178128+artemioadaysolvers@users.noreply.github.com>
2026-07-23 21:02:17 -0400

    Delete MJCF_Viewer directory

--- MJCF_Viewer/mjcf_viewer_main.js
+++
@@ -1,5 +0,0 @@
-// /MJCF_Viewer/mjcf_viewer_main.js
-// AutoMind BUILD230 MJCF entrypoint: X-Ray/Ghost/Section proxy synchronization.
-import { render, Base64Images } from './mjcf_viewer_main_core.js';
-export { render, Base64Images };
-export default { render };
