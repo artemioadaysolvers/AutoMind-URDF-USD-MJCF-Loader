1dfb0189ed928224bfccbeb740cbeba59a81614b
Artemio Araya Day <78178128+artemioadaysolvers@users.noreply.github.com>
2026-07-23 22:28:57 -0400

    Delete MJCF_Viewer directory

--- MJCF_Viewer/mjcf_viewer_main.js
+++
@@ -1,5 +0,0 @@
-// /MJCF_Viewer/mjcf_viewer_main.js
-// AutoMind BUILD238 MJCF entrypoint: pooled translucent hover, canonical assets, indexed picking and on-demand rendering.
-import { render, Base64Images } from './mjcf_viewer_main_core.js';
-export { render, Base64Images };
-export default { render };
